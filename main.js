9b5b7dbcff2508429ea23a3eb5f62bf462d31b47
roxnlopez <roxnlopez@gmail.com>
2017-01-25 15:46:42 -0700

    js hints
    insert hints to work from.

--- main.js
+++ main.js
@@ -25,7 +25,19 @@ document.getElementById("q0").classList.add("status-good");
 // 1. Without using Math.min(), write a function called minimum(),
 //    that takes two numbers from a user,
 //    and outputs the smaller number into the HTML page.
+function minimum(num1, num2) {
+  if(num1 > num2) {
+    return num1 + " is larger than " + num2;
+  } else if(num2 > num1) {
+    return num2 + " is larger than " + num1;
+  } else {
+    return "Jeez - those are equal or something. Don't get tricky."
+  }
+};
 
+console.log(minimum(3,4));
+console.log(minimum(5,2));
+console.log(minimum(4,4));
 
 
 
@@ -34,20 +46,54 @@ document.getElementById("q0").classList.add("status-good");
 //    to find the smallest of three numbers,
 //    input by a user,
 //    and the smallest displayed within the HTML page.
+function minimum3(num1, num2, num3) {
+  if (num1 >= num2 && num1 >= num3) {
+    return num1 + " is largest.";
+  } else if (num2 >= num1 && num2 >= num3) {
+    return num2 + " is largest.";
+  } else if (num3 >= num1 && num3 >= num2) {
+    return num3 + " is largest";
+  } else {
+    return "I'm not sure";
+  }
+};
 
+console.log(minimum3(3,4,5));
+console.log(minimum3(5,2,1));
+console.log(minimum3(4,6,2));
+console.log(minimum3(4,4,2));
+console.log(minimum3("horse", 3, null));
 
 
 
 // 3. Declare a function called sum() that takes an array of numbers as an argument adds them together and displays the result within the HTML page.
 //    i.e. sum([1, 2, 3, 4]) should return 10.
+function sum(numArray) {
+  var total = 0;
+  for (i=0; i<numArray.length; i++) {
+    total += numArray[i];
+  }
+  return total + " is the total";
+};
 
+console.log(sum([2,4,5]));
+console.log(sum([43,66,99,22]));
 
 
 
 
 // 4. Declare a function called multiply() that takes an array of numbers and multiplies them together.
 //    i.e. multiply([1, 2, 3, 4]) should return 24.
+function multiply(numArray) {
+  var total = 1;
+  for(i=0; i<numArray.length; i++) {
+    total *= numArray[i];
+  }
+  return total + " is the total.";
+};
 
+console.log(multiply([1,2,3,4]));
+console.log(multiply([43,66,99,22]));
 
 
 
@@ -57,6 +103,19 @@ document.getElementById("q0").classList.add("status-good");
 //    that are less than six characters.
 var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", "outlet"];
 
+var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", "outlet"];
+
+function filterSixPlus(array) {
+  for (i=0; i<array.length; i++) {
+    if (array[i].length < 6) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return array;
+}
+
+console.log(filterSixPlus(words));
 
 
 
@@ -64,7 +123,18 @@ var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", 
 
 // 6. Use a function to ask a user for a temperature in Celsius and converts it to Fahrenheit.
 //    HINT: You may need to use parseFloat to convert the user's string input to a float.
+var userInput = prompt("Please enter a degree to convert.");
 
+function getFarenheitFromC(userInput) {
+  // userInput = userInput;
+  console.log("userinput=", userInput)
+  userInput *= 1.8;
+  userInput += 32;
+  value = userInput.toFixed(0);
+  return value;
+}
+
+console.log(getFarenheitFromC(userInput));
 
 
 
@@ -74,14 +144,47 @@ var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", 
 //    or from Celsius to Fahrenheit. To keep it simple, instruct them to use "F to C" or "C to F".
 //    Display an error if they do not follow the rules.
 
+var userChoice = prompt("Please choose to convert Fahrenheit to Celsius by typing 'F to C' or Celsius to Fahrenheit by typing 'C to F'");
 
+function f2COrC2F(userChoice) {
+  if (userChoice === "C to F") {
+    var userInput = prompt("Please enter the degree.");
+    userInput = parseFloat(userInput);
+    userInput *= 1.8;
+    userInput += 32;
+    value = userInput.toFixed(0);
+    return value;
+  } else if (userChoice === "F to C") {
+    var userInput = prompt("Please enter the degree.");
+    userInput = parseFloat(userInput);
+    userInput -= 32;
+    userInput /= 1.8;
+    value = userInput.toFixed(0);
+    return value;
+  } else {
+    userChoice = prompt("You didn't pick F to C or C to F. TRY AGAIN!")
+    f2COrC2F(userChoice);
+  }
+}
+
+console.log(f2COrC2F(userChoice));
 
 
 // 8. Write a function countBs() that takes a string as its only argument and returns
 //    a number that indicates how many uppercase “B” characters are in the string.
 //    HINT: Google charAt()
 
+function countBs(string) {
+  var count = 0;
+  for (i=0; i<string.length; i++) {
+    if (string.charAt(i) === "B");
+      count += 1;
+  }
+  return count;
+}
 
+console.log(countBs("BBBBBBabbling babies are usually named Bonnie"));
+console.log(countBs("Bob Saget is a big weirdo."));
 
 
 
@@ -89,7 +192,7 @@ var words = ["window", "table", "cup", "knife", "barstool", "glass", "charger", 
 //    Have fun with these!
 //    Get as far as you can and record your progress and time.
 //    We'll try this again in a few weeks!
-
+ß
 
 
 
